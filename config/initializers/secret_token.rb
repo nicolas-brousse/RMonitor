f9208457924585ef34201452097ec83cccda39f5
Nicolas BROUSSE <pro@nicolas-brousse.fr>
2011-12-20 09:58:47 +0100

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RMonitor::Application.config.secret_token = 'b29fd1d6424de5ca23c7843a919f1cca34a1fda7be593b35c83cd13149418262bd89f1946391531c3b2402cdbb61dd8d849b7e6a6e04c3f6e5232124a33ac885'
