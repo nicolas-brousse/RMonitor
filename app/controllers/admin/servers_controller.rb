793102bd98ce4be77e3385a48cd42dccadaa99cd
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-07-12 01:19:09 +0200

    Update Servers CRUD

--- app/controllers/admin/servers_controller.rb
+++ app/controllers/admin/servers_controller.rb
@@ -1,31 +1,56 @@
 class Admin::ServersController < Admin::BaseController
+  before_filter :init_breadcrum, :except => [:create, :update, :destroy]
 
-  def initialize
-    super
-    add_breadcrumb "Servers", :admin_servers
-  end
-
+  # GET /admin/servers/
   def index
     @servers = Server.all
   end
 
+  # GET /admin/servers/:id
   def show
     @server = Server.find(params[:id])
     add_breadcrumb "#{@server.name} \##{@server.id}", admin_server_path(@server)
   end
 
+  # GET /admin/servers/new
   def new
+    add_breadcrumb "New server", new_admin_server_path()
+    @server = Server.new
   end
 
+  # POST /admin/servers/
   def create
+    @server = Server.new(params[:server])
+
+    respond_to do |format|
+      if @server.save
+        format.html { redirect_to admin_server_path(@server) }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
 
+  # GET /admin/servers/:id/edit
   def edit
+    @server = Server.find(params[:id])
+    add_breadcrumb "#{@server.name} \##{@server.id}", edit_admin_server_path(@server)
   end
 
+  # PUT /admin/servers/:id
   def update
+    @server = Server.find(params[:id])
+
+    respond_to do |format|
+      if @server.update_attributes(params[:server])
+        format.html { redirect_to admin_server_path(@server) }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
   end
 
+  # DELTE /admin/servers/:id
   def destroy
     @server = Server.find(params[:id])
     @server.destroy
@@ -35,4 +60,8 @@ class Admin::ServersController < Admin::BaseController
     end
   end
 
+private
+    def init_breadcrum
+      add_breadcrumb "Servers", admin_servers_path()
+    end
 end
--- app/helpers/admin/index_helper.rb
+++ app/helpers/admin/index_helper.rb
@@ -1,2 +1,13 @@
 module Admin::IndexHelper
+
+  def format_flash_class(type)
+    {
+      :success => "alert-success",
+      :error   => "alert-error",
+      :info    => "alert-info",
+      :alert   => "",
+      :notice  => "alert-info",
+    }[type]
+  end
+
 end
