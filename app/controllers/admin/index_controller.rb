7686817925a0f248a0e662a134f5d52d8c7bce59
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-07-12 19:50:52 +0200

    Clean views and css

--- app/controllers/admin/index_controller.rb
+++ app/controllers/admin/index_controller.rb
@@ -1,9 +1,11 @@
 class Admin::IndexController < Admin::BaseController
 
   def dashboard
+    add_breadcrumb "Dashboard", admin_dashboard_path()
   end
 
   def info
+    add_breadcrumb "Informations", admin_info_path()
   end
 
 end
