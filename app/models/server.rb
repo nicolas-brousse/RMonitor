14a1a6a93778221f19fc7e8bacb946f242c74464
Nicolas BROUSSE <pro@nicolas-brousse.fr>
2012-07-13 14:27:25 +0200

    Try to calculate server uptime

--- app/models/server.rb
+++ app/models/server.rb
@@ -5,4 +5,26 @@ class Server < ActiveRecord::Base
 
   # Named Scopes
   scope :publics, lambda{ where('true') } #where("is_public = ?", true) }
+
+  def uptime
+    out_time = 0
+    latest = nil
+
+    monitorings = self.monitorings
+    return null if monitorings.nil?
+
+    monitorings.order('created_at ASC').each do |m|
+      if !latest.nil? && latest.status != m.status
+        out_time += m.created_at - latest.created_at
+      end
+      latest = m
+    end
+
+    if !latest.status
+      out_time += Time.now - latest.created_at
+    end
+
+    total_time = Time.now - (self.monitorings.order('created_at DESC').last).created_at
+    100 - ((total_time - out_time) / total_time)
+  end
 end
