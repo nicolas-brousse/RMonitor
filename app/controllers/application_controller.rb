e1d94c379fddff6623d5bee3c794f8119b38e77d
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-07-19 01:05:20 +0200

    Prepare user controller for administration (#2)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
+  before_filter :set_locale
 
 protected
   def set_locale
-    I18n.locale = current_user.locale.to_sym
-    Time.zone = current_user.time_zone
+    # I18n.locale = current_user.locale.to_sym
+    # Time.zone = current_user.time_zone
+    # Time.zone = nil
   end
 
   def add_breadcrumb name, url = ''
