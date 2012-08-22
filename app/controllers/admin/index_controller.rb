8d8ae2db1b0e2fc6a9a674cdcf66a4b1c4f263bf
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-08-23 00:35:32 +0200

    Clean code

--- app/controllers/admin/index_controller.rb
+++ app/controllers/admin/index_controller.rb
@@ -20,7 +20,6 @@ class Admin::IndexController < ApplicationController
       Setting[k] = v
     end
 
-
     respond_to do |format|
       format.html { redirect_to :admin_settings, :notice => :settings_updated }
       format.js   { flash.now[:notice] = :settings_updated }
