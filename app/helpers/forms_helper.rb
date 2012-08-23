beb877ce86c116683e645d610f123087fa8a62b8
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-08-23 19:08:28 +0200

    Update cancel_tag

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,5 +1,11 @@
 module FormsHelper
-  def cancel_tag
-    link_to(t(:cancel), :back, :data => {:confirm => "Are you sure?"}, :class => "btn")
+  def cancel_tag(url=nil)
+    if url.nil?
+      url = :back 
+    else
+      url = url_for(url)
+    end
+
+    link_to(t(:cancel), url, :data => {:confirm => "Are you sure?"}, :class => "btn")
   end
 end
