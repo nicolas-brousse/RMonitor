20251c39ff6dfe4584f2b9b042199a9b7dc13337
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-10-02 22:25:51 +0200

    Debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ protected
   def set_locale
     # TODO, verif if the language exist
     # I18n.locale = current_user.locale.to_sym || Setting.default_language.to_sym || I18n.locale
-    Time.zone = current_user.preferences.time_zone if user_signed_in?
+    Time.zone = current_user.preferences.try(:time_zone) if user_signed_in?
     # Time.zone = nil
   end
 end
