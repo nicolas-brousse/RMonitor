1838fee0701c4c3000c8b36d4406a8cd371a5992
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-10-03 21:42:17 +0200

    Update helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
+require 'digest/md5'
+
 module UsersHelper
+  def gravatar_url(user, size="40")
+    hash = Digest::MD5.hexdigest(user.email.downcase)
+    "http://www.gravatar.com/avatar/#{hash}?s=#{size.to_i}"
+  end
 end
