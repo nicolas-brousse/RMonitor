e2760bbc491c6e531473443d3f2b65d118dd4558
Nicolas Brousse <pro@nicolas-brousse.fr>
2012-10-18 23:59:04 +0200

    Update start and stop command with daemon_controller

--- lib/rmonitor/commands/start_command.rb
+++ lib/rmonitor/commands/start_command.rb
@@ -1,3 +1,5 @@
+require 'daemon_controller'
+
 module RMonitor
   module Commands
     class StartCommand < Base
@@ -10,25 +12,46 @@ module RMonitor
       def run
         shell.say "Start RMonitor", :green
         shell.say "--------------------------------"
-        # exec "bundle exec rails s"
+        # system "bundle exec rails s"
 
         # Start Redis server
         skip_redis_server = false
         unless skip_redis_server
-          shell.say "  Run Redis server"
-          exec "bundle exec redis start"
+          shell.say ">>  Run Redis server", :yellow
+          system "bundle exec redis start"
         end
 
         # Start Sidekiq
-        shell.say "  Run Sidekiq"
-        exec "bundle exec sidekiq"
+        shell.say ">>  Run Sidekiq", :yellow
+        sidekiq_daemon.start
 
         # Start Rails server (if not skiped)
         skip_rails_server = false
         unless skip_rails_server
-          shell.say "  Run Rails server"
-          exec "bundle exec rails s"
+          shell.say ">>  Run Rails server", :yellow
+          rails_daemon.start
         end
+      end
+
+    private
+      def sidekiq_daemon
+        @sidekiq_daemon ||= DaemonController.new(
+          :identifier => 'Sidekiq server',
+          :start_command => "bundle exec sidekiq -",
+          :ping_command => lambda { TCPSocket.new('localhost', @port) },
+          :pid_file => "#{Rails.root}/tmp/pids/sidekiq.pid",
+          :log_file => "#{Rails.root}/log/sidekiq.log",
+        )
+      end
+
+      def rails_daemon(port=3000)
+        @rails_daemon ||= DaemonController.new(
+          :identifier => 'Rails server',
+          :start_command => "bundle exec rails s -p #{port}",
+          :ping_command => lambda { TCPSocket.new('localhost', port) },
+          :pid_file => "#{Rails.root}/tmp/pids/sidekiq.pid",
+          :log_file => "#{Rails.root}/log/sidekiq.log",
+        )
       end
 
     end
--- lib/rmonitor/commands/stop_command.rb
+++ lib/rmonitor/commands/stop_command.rb
@@ -1,3 +1,5 @@
+require 'daemon_controller'
+
 module RMonitor
   module Commands
     class StopCommand < Base
@@ -6,7 +8,48 @@ module RMonitor
                        :description => "Stop  RMonitor"
 
       def run
-        puts "stop"
+        shell.say "Stop RMonitor", :green
+        shell.say "--------------------------------"
+        # system "bundle exec rails s"
+
+        # Start Redis server
+        skip_redis_server = false
+        unless skip_redis_server
+          shell.say ">>  Stop Redis server", :yellow
+          system "bundle exec redis stop"
+        end
+
+        # Start Sidekiq
+        shell.say ">>  Stop Sidekiq", :yellow
+        sidekiq_daemon.stop
+
+        # Start Rails server (if not skiped)
+        skip_rails_server = false
+        unless skip_rails_server
+          shell.say ">>  Stop Rails server", :yellow
+          rails_daemon.stop
+        end
+      end
+
+    private
+      def sidekiq_daemon
+        @sidekiq_daemon ||= DaemonController.new(
+          :identifier => 'Sidekiq server',
+          :start_command => "bundle exec sidekiq -",
+          :ping_command => lambda { TCPSocket.new('localhost', @port) },
+          :pid_file => "#{Rails.root}/tmp/pids/sidekiq.pid",
+          :log_file => "#{Rails.root}/log/sidekiq.log",
+        )
+      end
+
+      def rails_daemon(port=3000)
+        @rails_daemon ||= DaemonController.new(
+          :identifier => 'Rails server',
+          :start_command => "bundle exec rails s -p #{port}",
+          :ping_command => lambda { TCPSocket.new('localhost', port) },
+          :pid_file => "#{Rails.root}/tmp/pids/sidekiq.pid",
+          :log_file => "#{Rails.root}/log/sidekiq.log",
+        )
       end
 
     end
